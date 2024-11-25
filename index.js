e3b292d419e9bba4667c91693ab03ee3c0009449
Shivam Ishwar <102852506+shivamishwar07@users.noreply.github.com>
2024-11-25 15:46:34 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.get('/login', (req, res) => {
+    res.send('Welcome to login page');
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
